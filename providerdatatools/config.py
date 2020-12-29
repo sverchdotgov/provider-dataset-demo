3ff7b71ba342bf6d18f5294c71d4ac37b08ef246
Shaun Verch <shaun@shaunverch.com>
2020-12-29 18:22:00 -0500

    Factoring out more useful utilities

--- providerdatatools/config.py
+++ providerdatatools/config.py
@@ -2,6 +2,9 @@
 
 from datetime import datetime
 from jinja2 import Template
+import os
+import yaml
+import paths
 
 # TODO: Actually make this a config object that can return this URL and metadata. Even better, just
 # use an existing "yaml config" python library and override these fields.
@@ -21,3 +24,22 @@ def build_metadata(organization_type, provider_subset, datastore, dataset):
 def render_url(config, metadata):
     template = Template(config['url'])
     return template.render(metadata)
+
+def get_config_file(config_directory):
+    return os.path.join(config_directory, "config.yaml")
+
+def iterate_datasets():
+    config_root = paths.get_dataset_configuration_root()
+    for organization_type in os.listdir(config_root):
+        organization_path = os.path.join(config_root, organization_type)
+        for provider_subset in os.listdir(organization_path):
+            provider_subset_path = os.path.join(organization_path, provider_subset)
+            for datastore in os.listdir(provider_subset_path):
+                datastore_path = os.path.join(provider_subset_path, datastore)
+                for dataset in os.listdir(datastore_path):
+                    dataset_path = os.path.join(datastore_path, dataset)
+                    config_file = get_config_file(dataset_path)
+                    with open(config_file) as f:
+                        config = yaml.load(f, Loader=yaml.FullLoader)
+                    metadata = build_metadata(organization_type, provider_subset, datastore, dataset)
+                    yield dataset_path, config, metadata
--- providerdatatools/download.py
+++ providerdatatools/download.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python
 
 import os
-import yaml
 import sys
 from shutil import copyfile
 import paths, download_utils, extract_utils
 import config as config_utils
 
-config_root = paths.get_dataset_configuration_root()
-for organization_type in os.listdir(config_root):
-    organization_path = os.path.join(config_root, organization_type)
-    for provider_subset in os.listdir(organization_path):
-        provider_subset_path = os.path.join(organization_path, provider_subset)
-        for datastore in os.listdir(provider_subset_path):
-            datastore_path = os.path.join(provider_subset_path, datastore)
-            for dataset in os.listdir(datastore_path):
-                dataset_path = os.path.join(datastore_path, dataset)
-                config_file = os.path.join(dataset_path, "config.yaml")
-                with open(config_file) as f:
-                    config = yaml.load(f, Loader=yaml.FullLoader)
-                metadata = config_utils.build_metadata(organization_type, provider_subset, datastore, dataset)
-                url = config_utils.render_url(config, metadata)
-                download_directory = download_utils.build_download_directory(metadata)
-                os.makedirs(download_directory, exist_ok=True)
-                if "compression" in config:
-                    download_path = os.path.join(download_directory, "dataset.%s" % config["compression"])
-                    download_utils.download_with_progress(download_path, url)
-                    if config["compression"] != "zip":
-                        print("Unsupported compression: %s!" % config["compression"])
-                        sys.exit(1)
-                    extract_utils.unzip(download_path, download_directory)
-                else:
-                    download_path = os.path.join(download_directory, "dataset.%s" % config["format"])
-                    download_utils.download_with_progress(download_path, url)
-                copyfile(config_file, os.path.join(download_directory, "config.yaml"))
+for config_directory, config, metadata in config_utils.iterate_datasets():
+    url = config_utils.render_url(config, metadata)
+    download_directory = download_utils.build_download_directory(metadata)
+    os.makedirs(download_directory, exist_ok=True)
+    if "compression" in config:
+        download_path = os.path.join(download_directory, "dataset.%s" % config["compression"])
+        download_utils.download_with_progress(download_path, url)
+        if config["compression"] != "zip":
+            print("Unsupported compression: %s!" % config["compression"])
+            sys.exit(1)
+        extract_utils.unzip(download_path, download_directory)
+    else:
+        download_path = os.path.join(download_directory, "dataset.%s" % config["format"])
+        download_utils.download_with_progress(download_path, url)
+    src_config_file = config_utils.get_config_file(config_directory)
+    dest_config_file = config_utils.get_config_file(download_directory)
+    print("Copying config file from %s to %s" % (src_config_file, dest_config_file))
+    copyfile(src_config_file, dest_config_file)
